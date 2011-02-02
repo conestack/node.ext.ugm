20935e1e264961e448ea4f4b6450e2058c6dfa0a
Florian Friesdorf <flo@chaoflow.net>
2011-02-02 01:55:20 +0100

    factored out attraliaser, removed ldap stuff

--- src/node/ext/ugm/users.py
+++ src/node/ext/ugm/users.py
@@ -1,46 +1,26 @@
 from node.aliasing import DictAliaser
 from node.base import AbstractNode
 from node.bbb import AttributedNode
+from node.parts import AliasAttrs
 from zope.interface import implements
-
-from node.ext.ldap import LDAPProps, LDAPNode
-from node.ext.ldap import BASE, ONELEVEL, SUBTREE
-from node.ext.ldap.debug import debug
-from node.ext.ldap.interfaces import ILDAPUsersConfig
-from node.ext.ldap.interfaces import ILDAPGroupsConfig
 
 
 class Principal(Part):
     """Turns a node into a principal
     """
-    attraliaser = default(None)
-
-    @plumb
-    def __init__(_next, self, *args, **kw):
-        if attraliaser in kw:
-            self.attraliaser = kw.pop('attraliaser')
-        _next(self, *args, **kw)
-
     @extend
     @property
     def id(self):
         return self.__name__
 
-    @plumb
-    @property
-    def attrs(_next, self):
-        if self.attraliaser is None:
-            return _next(self)
-        aliased = AliasedNodespace(self.context.attrs, self.attraliaser)
-        aliased.allow_non_node_childs = True
-        return aliased
-
+    @extend
     def __repr__(self):
         return "<%s '%s'>" % (
                 self.__class__.__name__.split('.')[-1],
                 unicode(self.id).encode('ascii', 'replace'),
                 )
 
+    @extend
     def __iter__(self):
         return iter([])
 
@@ -171,115 +151,3 @@ class Principals(AbstractNode):
         aliased_results = \
                 [(id, self._alias_dict(attrs)) for id, attrs in results]
         return aliased_results
-
-
-class LDAPPrincipalsConfig(object):
-    """Superclass for LDAPUsersConfig and LDAPGroupsConfig
-    """
-    def __init__(self,
-            baseDN='',
-            attrmap={},
-            scope=ONELEVEL,
-            queryFilter='',
-            objectClasses=[]):
-        self.baseDN = baseDN
-        self.attrmap = attrmap
-        self.scope = scope
-        self.queryFilter = queryFilter
-        self.objectClasses = objectClasses
-
-
-class LDAPUsersConfig(LDAPPrincipalsConfig):
-    """Define how users look and where they are
-    """
-    implements(ILDAPUsersConfig)
-    
-    #when a user is modified, killed etc an event is emmited. To grab it you must:
-    #zope.component.provideHandler(funct_to_be_executed,[1st_arg_objecttype,2nd_arg_objecttype,..])
-
-
-class LDAPGroupsConfig(LDAPPrincipalsConfig):
-    """Define how groups look and where they are
-    """
-    implements(ILDAPGroupsConfig)
-
-
-class LDAPPrincipals(Principals):
-    """Superclass for LDAPUsers and LDAPGroups
-    """
-    def __init__(self, props, cfg):
-        context = LDAPNode(name=cfg.baseDN, props=props)
-        super(LDAPPrincipals, self).__init__(context, cfg.attrmap)
-        self.context._child_filter = cfg.queryFilter
-        self.context._child_scope = int(cfg.scope)
-        self.context._child_objectClasses = cfg.objectClasses
-        self.context._key_attr = cfg.attrmap['id']
-        self.context._rdn_attr = cfg.attrmap['rdn']
-
-
-class LDAPUsers(LDAPPrincipals):
-    """Manage LDAP users
-    """
-    principal_factory = User
-
-    def __init__(self, props, cfg):
-        super(LDAPUsers, self).__init__(props, cfg)
-        if cfg.attrmap['login'] != cfg.attrmap['id']:
-            self.context._seckey_attrs = (cfg.attrmap['login'],)
-
-    # XXX: do we really need this?
-    # XXX: login is a mapped attr, we could simply search on it
-    def idbylogin(self, login):
-        """Return the users id or raise KeyError
-        """
-        self.context.keys()
-        if self.principal_attrmap['login'] == self.principal_attrmap['id']:
-            if login not in self:
-                raise KeyError(login)
-            # XXX: Is this sane, or should we tell that they are the same?
-            return login
-        return self.context._seckeys[self.principal_attrmap['login']][login]
-
-    @debug(['authentication'])
-    def authenticate(self, login=None, pw=None, id=None):
-        """Authenticate a user either by id xor by login
-
-        If successful, the user's id is returned, otherwise None
-        """
-        if id is not None and login is not None:
-            raise ValueError(u"Either specify id or login, not both.")
-        if pw is None:
-            raise ValueError(u"You need to specify a password")
-        if login:
-            try:
-                id = self.idbylogin(login)
-            except KeyError:
-                return None
-        try:
-            userdn = self.context.child_dn(id)
-        except KeyError:
-            return None
-        return self.context._session.authenticate(userdn, pw) and id or None
-
-    @debug(['authentication'])
-    def passwd(self, id, oldpw, newpw):
-        """Change a users password
-        """
-        self.context._session.passwd(self.context.child_dn(id), oldpw, newpw)
-
-
-class LDAPGroups(LDAPPrincipals):
-    """Manage LDAP groups
-
-    XXX
-        for groups children are found by:
-        - we have a multivalue attribute pointing to member dns
-        - has an attribute pointing to our dn
-        - we have an attribute that matches another attribute on the user
-
-        AD: dn:memberOf
-        openldap: member:dn
-        posix: memberUid:uidNumber|gidNumber:gidNumber
-        arbitrary: group_attr:user_attr  |   &    ()
-    """
-    principal_factory = Group
