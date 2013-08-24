cfb41d6a34fb34fe653801ef1cc4ecd7c7448a74
Robert Niederreiter <office@squarewave.at>
2013-08-24 11:25:27 +0200

    minor cleanup

--- setup.py
+++ setup.py
@@ -1,26 +1,31 @@
-from setuptools import setup, find_packages
-import sys, os
+import os
+from setuptools import (
+    setup,
+    find_packages,
+)
+
 
 version = '0.9.3'
 shortdesc = "Node-based user and group management"
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 longdesc += open(os.path.join(os.path.dirname(__file__), 'LICENSE.rst')).read()
 
+
 setup(name='node.ext.ugm',
       version=version,
       description=shortdesc,
       long_description=longdesc,
       classifiers=[
-            'Environment :: Web Environment',
-            'Programming Language :: Python',
-      ], # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+          'Environment :: Web Environment',
+          'Programming Language :: Python',
+      ],
       keywords='node user group role',
       author='BlueDynamics Alliance',
       author_email='dev@bluedynamics.com',
       url='https://github.com/bluedynamics/node.ext.ugm',
       license='Simplified BSD',
       packages=find_packages('src'),
-      package_dir = {'': 'src'},
+      package_dir={'': 'src'},
       namespace_packages=['node', 'node.ext'],
       include_package_data=True,
       zip_safe=False,
