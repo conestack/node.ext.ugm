4665dea586c8eff5a39a0780a583e77191d5b6f6
Robert Niederreiter <office@squarewave.at>
2017-03-14 08:45:40 +0100

    "setup.py indentation"

--- setup.py
+++ setup.py
@@ -9,36 +9,37 @@ longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 longdesc += open(os.path.join(os.path.dirname(__file__), 'LICENSE.rst')).read()
 
 
-setup(name='node.ext.ugm',
-      version=version,
-      description=shortdesc,
-      long_description=longdesc,
-      classifiers=[
-          'Environment :: Web Environment',
-          'Programming Language :: Python',
-      ],
-      keywords='node user group role',
-      author='BlueDynamics Alliance',
-      author_email='dev@bluedynamics.com',
-      url='https://github.com/bluedynamics/node.ext.ugm',
-      license='Simplified BSD',
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      namespace_packages=['node', 'node.ext'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'node',
-          'plumber',
-          'setuptools',
-      ],
-      extras_require={
-          'test': [
-              'interlude',
-              'unittest2',
-              'zope.testing',        
-          ]
-      },
-      entry_points="""
-      """,
-      )
+setup(
+    name='node.ext.ugm',
+    version=version,
+    description=shortdesc,
+    long_description=longdesc,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Programming Language :: Python',
+    ],
+    keywords='node user group role',
+    author='BlueDynamics Alliance',
+    author_email='dev@bluedynamics.com',
+    url='https://github.com/bluedynamics/node.ext.ugm',
+    license='Simplified BSD',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['node', 'node.ext'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'node',
+        'plumber',
+        'setuptools',
+    ],
+    extras_require={
+        'test': [
+            'interlude',
+            'unittest2',
+            'zope.testing',
+        ]
+    },
+    entry_points="""
+    """
+)
