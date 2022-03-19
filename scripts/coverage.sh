7f15635daec69ca08ed36e1fdc3fc9576f951ca0
Robert Niederreiter <rnix@squarewave.at>
2022-03-19 13:10:39 +0100

    Update helper scripts

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,5 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
-./$1/bin/coverage run --source=src/node/ext/ugm -m node.ext.ugm.tests.__init__
-./$1/bin/coverage report
-./$1/bin/coverage html
+function run_coverage {
+    local target=$1
+
+    if [ -e "$target" ]; then
+        ./$target/bin/coverage run \
+            --source=src/node/ext/ugm \
+            -m node.ext.ugm.tests.__init__
+        ./$target/bin/coverage report
+    else
+        echo "Target $target not found."
+    fi
+}
+
+run_coverage py2
+run_coverage py3
+run_coverage pypy3
